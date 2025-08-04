e7fa25a4c2fb9f34efa32eb035af350df6aab2d8
jo <jtcaraballm@gmail.com>
2025-08-04 14:55:30 -0400

    feat: add working layer impl.

--- rkb2040-lib/src/keymap.rs
+++ rkb2040-lib/src/keymap.rs
@@ -79,6 +79,7 @@ impl KeyState {
 }
 
 struct OneShotSM {
+    pressed: u8,
     active: u8,
     used: u8,
 }
@@ -87,7 +88,11 @@ impl OneShotSM {
     #[inline]
     #[must_use]
     pub const fn new() -> Self {
-        Self { active: 0, used: 0 }
+        Self {
+            pressed: 0,
+            active: 0,
+            used: 0,
+        }
     }
 
     #[inline]
@@ -98,17 +103,22 @@ impl OneShotSM {
     #[inline]
     pub fn add(&mut self, m: u8) {
         self.active |= m;
+        self.pressed |= m;
     }
 
     #[inline]
     pub fn remove_if_used(&mut self, m: u8) {
-        // (self.used & m) instead of just (m) allows to skip checking for if self.used & m != 0.
-        self.active &= 0b1111_1111 ^ (self.used & m);
+        self.pressed &= 0b1111_1111 ^ m;
+        if self.used & m == 0 {
+            return;
+        }
+        self.active &= 0b1111_1111 ^ m;
+        self.used &= 0b1111_1111 ^ m;
     }
 
     #[inline]
     pub fn apply<const N: usize>(&mut self, seq: &mut Seq<Keyboard, N>) {
-        self.used |= self.active;
+        self.used |= self.active & self.pressed;
         let mut curr_mod: u8 = 0xE0; // 0xE0 - 0xE7 correspond to modifier keycodes.
         while self.active != 0 {
             if self.active & OS_MASK != 0 {
@@ -120,13 +130,67 @@ impl OneShotSM {
     }
 }
 
+struct LayerSM {
+    head: usize,
+    stack: [usize; 256],
+}
+
+impl LayerSM {
+    const LIMIT: usize = 1;
+
+    #[inline]
+    #[must_use]
+    pub const fn new() -> Self {
+        Self {
+            head: 0,
+            stack: [0; 256],
+        }
+    }
+
+    #[inline]
+    pub fn current_layer(&self) -> usize {
+        self.stack[self.head]
+    }
+
+    #[inline]
+    pub fn add(&mut self, v: usize) {
+        self.head += 1;
+        self.stack[self.head] = v;
+    }
+
+    #[inline]
+    // Sadly we need to accommodate for a layer other than the top most one to be removed.
+    pub fn remove(&mut self, v: usize) {
+        let mut s = 0;
+        for i in (Self::LIMIT..=self.head).rev() {
+            if self.stack[i] == v {
+                s = i;
+                break;
+            }
+        }
+        if s == 0 {
+            return;
+        }
+        if s != self.head {
+            unsafe {
+                let ptr = self.stack.as_mut_ptr();
+                let src_ptr = ptr.add(s + 1);
+                let dest_ptr = ptr.add(s);
+                core::ptr::copy(src_ptr, dest_ptr, self.head - s);
+            }
+        }
+        self.head -= 1;
+    }
+}
+
 pub struct KeymapSM<const N: usize, const M: usize> {
     key_state: [KeyState; N],
     keymap: [[Keybind; N]; M],
     curr_scan: Seq<Keyboard, SEQUENCE_LIMIT>,
     scans: Queue<Seq<Keyboard, SEQUENCE_LIMIT>, REPORT_QUEUE_LIMIT>,
-    layer: usize,
+    layer_stack: LayerSM,
     one_shots: OneShotSM,
+    layer: usize,
 }
 
 impl<const N: usize, const M: usize> KeymapSM<N, M> {
@@ -138,12 +202,14 @@ impl<const N: usize, const M: usize> KeymapSM<N, M> {
             scans: Queue::new(),
             key_state: core::array::from_fn(|_| KeyState::new()),
             layer: 0,
+            layer_stack: LayerSM::new(),
             one_shots: OneShotSM::new(),
         }
     }
 
     #[inline]
     pub fn begin_scan(&mut self) {
+        self.layer = self.layer_stack.current_layer();
         self.curr_scan.reset();
     }
 
@@ -155,9 +221,13 @@ impl<const N: usize, const M: usize> KeymapSM<N, M> {
         if state.pressed == pressed {
             return;
         }
+        // Only change keybind when pressed.
+        if pressed {
+            state.last = self.keymap[self.layer][pos];
+        }
+        // No ops should not trigger changes on being pressed or being released.
         state.changed = true;
         state.pressed = pressed;
-        state.last = self.keymap[self.layer][pos];
     }
 
     #[inline]
@@ -169,41 +239,44 @@ impl<const N: usize, const M: usize> KeymapSM<N, M> {
                 Keybind::NoOP => {}
                 Keybind::Key(k) => {
                     scan_is_reportable |= state.changed;
-                    state.changed = false;
-                    if !state.pressed {
-                        continue;
+                    if state.pressed {
+                        non_mod_in_scan = true;
+                        let _ = self.curr_scan.add(k);
                     }
-                    non_mod_in_scan = true;
-                    let _ = self.curr_scan.add(k);
                 }
                 Keybind::Mod(m) => {
                     scan_is_reportable |= state.changed;
-                    state.changed = false;
-                    if !state.pressed {
-                        continue;
+                    if state.pressed {
+                        let _ = self.curr_scan.add(m.to_key());
                     }
-                    let _ = self.curr_scan.add(m.to_key());
                 }
                 Keybind::Combo(m, k) => {
                     scan_is_reportable |= state.changed;
-                    state.changed = false;
-                    if !state.pressed {
-                        continue;
+                    if state.pressed {
+                        non_mod_in_scan = true;
+                        let _ = self.curr_scan.add(m.to_key());
+                        let _ = self.curr_scan.add(k);
                     }
-                    non_mod_in_scan = true;
-                    let _ = self.curr_scan.add(m.to_key());
-                    let _ = self.curr_scan.add(k);
                 }
                 Keybind::OneShot(m) => {
-                    state.changed = false;
-                    if !state.pressed {
+                    if state.pressed {
+                        self.one_shots.add(m.to_byte());
+                    } else if state.changed {
                         self.one_shots.remove_if_used(m.to_byte());
+                    }
+                }
+                Keybind::Layer(l) => {
+                    if !state.changed {
                         continue;
                     }
-                    self.one_shots.add(m.to_byte());
+                    if state.pressed {
+                        self.layer_stack.add(l as usize);
+                    } else {
+                        self.layer_stack.remove(l as usize);
+                    }
                 }
-                Keybind::Layer(_) => {}
             }
+            state.changed = false;
         }
         if !scan_is_reportable {
             return;
@@ -266,5 +339,19 @@ impl_mods!(
 macro_rules! KC {
     ($key:tt) => {
         $crate::keymap::Keybind::Key(usbd_human_interface_device::page::Keyboard::$key)
+    };
+}
+
+#[macro_export]
+macro_rules! MO {
+    ($layer:tt) => {
+        $crate::keymap::Keybind::Layer($layer)
+    };
+}
+
+#[macro_export]
+macro_rules! NA {
+    () => {
+        $crate::keymap::Keybind::NoOP
     };
 }
